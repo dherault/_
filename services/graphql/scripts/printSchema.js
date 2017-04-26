061fdc33eb1c8adddd58ec9ee61ccfae8ef0d0ab
David Hérault <dherault@gmail.com>
2017-04-26 18:22:02 +0200

    add relay compiler

---
+++ services/graphql/scripts/printSchema.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+const fs = require('fs');
+const path = require('path');
+const { graphql } = require('graphql');
+const { printSchema } = require('graphql/utilities');
+
+const schema = require('../schema');
+const out = path.join(__dirname, '../../../dist/schema.graphql');
+
+// Save user readable type system shorthand of schema
+fs.writeFileSync(out, printSchema(schema));
